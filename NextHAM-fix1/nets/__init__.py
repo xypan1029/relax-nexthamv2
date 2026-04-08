81207c7e262434fd912044a23ce25ab25ecbb32b
zhaosheng <thesheng@mail.ustc.edu.cn>
2026-04-08 15:32:03 +0800

    Further trim unused model variants and aux code

--- NextHAM-fix1/nets/__init__.py
+++ NextHAM-fix1/nets/__init__.py
@@ -1,5 +1,4 @@
 from .registry import model_entrypoint
 
 from .graph_attention_transformer import *
-from .nextham import *
 from .hmodel import *
--- equiformer_ef/nets/__init__.py
+++ equiformer_ef/nets/__init__.py
@@ -1,16 +1,4 @@
 from .registry import model_entrypoint
 
-from .graph_attention_transformer import *
-from .graph_attention_transformer_md17 import *
-from .graph_attention_transformer_oc20 import *
 from .fmodel import *
-from .fmodel_bak import *
-from .hmodel import *
-from .zmodel import *
-from .model_jit import *
-
-from .dp_attention_transformer import *
-from .dp_attention_transformer_md17 import *
-from .dp_attention_transformer_oc20 import *
-
 
